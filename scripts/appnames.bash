92202918b2211d38a1f50c37e5cc7f93f0d988c4
ipad_kid <ipadkid358@gmail.com>
2017-03-17 20:07:03 -0400

    Update

---
+++ scripts/appnames.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cd /Applications
+
+filecontent=( `ls .`)
+
+for t in "${filecontent[@]}"
+do
+cd "$t"
+
+test "$(plutil -key CFBundleIcons Info.plist)" != "" && { 
+test "$(plutil -key SBAppTags Info.plist)" = "" && { 
+
+test "$(plutil -key CFBundleName Info.plist)" != "" && { echo -n "Name: "; plutil -key CFBundleName Info.plist; }
+
+test "$(plutil -key CFBundleDisplayName Info.plist)" != "" && { echo -n "Display Name: "; plutil -key CFBundleDisplayName Info.plist; }
+
+test "$(plutil -key CFBundleExecutable Info.plist)" != "" && { echo -n "Executable Name: "; plutil -key CFBundleExecutable Info.plist; }
+
+test "$(plutil -key CFBundleIdentifier Info.plist)" != "" && { 
+echo -n "Bundle ID: " 
+plutil -key CFBundleIdentifier Info.plist 
+echo 
+echo ———————
+echo
+      }
+   }
+}
+cd /Applications
+done
+
+
+cd /var/containers/Bundle/Application
+
+filecontent=( `ls .`)
+
+for t in "${filecontent[@]}"
+do
+cd "$t"
+cd *.app
+
+test "$(plutil -key CFBundleName Info.plist)" != "" && { echo -n "Name: "; plutil -key CFBundleName Info.plist; }
+
+test "$(plutil -key CFBundleDisplayName Info.plist)" != "" && { echo -n "Display Name: "; plutil -key CFBundleDisplayName Info.plist; }
+
+test "$(plutil -key CFBundleExecutable Info.plist)" != "" && { echo -n "Executable Name: "; plutil -key CFBundleExecutable Info.plist; }
+
+test "$(plutil -key CFBundleIdentifier Info.plist)" != "" && { 
+echo -n "Bundle ID: " 
+plutil -key CFBundleIdentifier Info.plist 
+echo 
+echo ———————
+echo
+}
+cd /var/containers/Bundle/Application
+done
