41ca448e21c82f9ce0ed0bc1df766d1eb2a83fd1
ipad_kid <ipadkid358@gmail.com>
2017-03-26 11:47:51 -0400

    File clean up, increased input validation for redeb.sh, added simple tweaklist.sh script, up dated scripts/index.html to refelect changes

--- scripts/redeb.sh
+++ scripts/redeb.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-test "$1" = "" && { echo Please rerun this script with a package id; }
-test "$1" != "" && {
-mkdir redebs
+test "$1" = "" && {
+echo Please rerun this script with a package id
+exit
+} || {
+test "$1" != "$(grep "Package: " /var/lib/dpkg/status | cut -c 10- | grep -v "gsc." | grep $1)" && { 
+echo Please rerun this script with a valid package id
+echo You may have meant: 
+grep "Package: " /var/lib/dpkg/status | cut -c 10- | grep -v "gsc." | grep $1
+exit
+} || {
+test -d redebs || mkdir redebs
 cd redebs
 mkdir sandbox
 mkdir sandbox/DEBIAN
@@ -10,7 +18,7 @@ for t in `ls /var/lib/dpkg/info | grep $1 | grep -v "$1".list | grep -v "$1".md5
 do
 cp /var/lib/dpkg/info/$1.$t sandbox/DEBIAN/$t
 done
-cat /var/lib/dpkg/status | grep -A 20 "Package: $1" | grep -v "Status: " | awk '/^$/{exit} {print $0}' > sandbox/DEBIAN/control
+grep -A 20 "Package: $1" /var/lib/dpkg/status | grep -v "Status: " | awk '/^$/{exit} {print $0}' > sandbox/DEBIAN/control
 for t in `cat /var/lib/dpkg/info/"$1".list | tail -n+2 | sed  's .  '`
 do
 test -d /$t && mkdir sandbox/$t
@@ -18,8 +26,9 @@ test -f /$t && cp /$t sandbox/$t
 done
 dpkg-deb -b sandbox "$1".deb > /dev/null
 tput bold
-echo Success!!
-echo The deb should be found in a folder called redebs
+echo Success!! The deb should be located at $(pwd)/$1.deb
 tput sgr0
-rm -rf sandbox 
+rm -rf sandbox
+exit
+}
 }
