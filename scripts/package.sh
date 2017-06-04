1fbc87125e1a4bded75311e1a853c0b8327764dd
ipad_kid <ipadkid358@gmail.com>
2017-06-04 11:16:07 -0400

    Update

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+{
 ls $1 | while read d
 do
 p="$1"/"$d"
-dpkg-deb -f $p >> Packages
-echo "Filename: $p" >> Packages
-echo "MD5sum: $(md5sum $p | cut -d " " -f1)" >> Packages
-echo "Size: $(stat -c %s $p)" >> Packages
-echo >> Packages
+dpkg-deb -f $p 
+echo "Filename: $p" 
+echo "MD5sum: $(md5sum $p | cut -d " " -f1)" 
+echo "Size: $(stat -c %s $p)"
+echo
 done
+} | bzip2 -zc > Packages.bz2
