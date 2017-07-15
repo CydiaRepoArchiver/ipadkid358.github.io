36ee075852ddb43dddccbd27596305607a37eb93
ipad_kid <ipadkid358@gmail.com>
2017-07-15 12:29:49 -0400

    Some ftt updates and fixes. TeamID script fix. Got rid of repo and testing stuff

--- scripts/teamid.sh
+++ scripts/teamid.sh
@@ -8,6 +8,6 @@ cd /var/containers/Bundle/Application/"$t"/*.app
 test "$(plutil -key CFBundleName Info.plist)" = "yalu102" && {
 tail -n+4 embedded.mobileprovision > $T
 test "$(plutil -key ProvisionsAllDevices $T)" = "" && plutil -key TeamIdentifier $T | tail -n+2 | head -n-1 | sed -e 's/[\t ]//g'
-rm $T
 }
+rm $T
 done
